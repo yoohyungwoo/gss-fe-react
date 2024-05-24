df77459a346fbb0b714e62246f5d73fc13e65665
h.0.0.w <dbguddn97@naver.com>
2024-05-24 15:53:00 +0900

    [feat] : generouter 추가

---
+++ src/pages/index.tsx
@@ -0,0 +1,3 @@
+export default function Home() {
+  return <div><h2>test</h2></div>
+}
--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,12 @@
 import * as path from "path"
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
+import generouted from '@generouted/react-router/plugin'
+
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), generouted()],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
